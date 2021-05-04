979f373934361b77354a708ec4808c358ba415ef
Anastasya <aamokeeva@yandex.ru>
2021-05-04 22:06:59 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+// утилитарный модуль - экспортирует общие функции и переменные для всех модулей
+(function () {
+    window.utils = {
+        KeyCode: {
+            BACKSPACE: 'Backspace',
+            ESCAPE: 'Escape'
+        }
+    };
+}());
+// мобильное меню - начиная с планшетной версии
+(function () {
+    var pageHeader = document.querySelector('.page-header');
+    var headerToggle = document.querySelector('.page-header__toggle');
+    if (pageHeader && headerToggle) {
+        pageHeader.classList.remove('page-header--nojs');
+        headerToggle.addEventListener('click', function () {
+            if (pageHeader.classList.contains('page-header--closed')) {
+                pageHeader.classList.remove('page-header--closed');
+                pageHeader.classList.add('page-header--opened');
+            } else {
+                pageHeader.classList.add('page-header--closed');
+                pageHeader.classList.remove('page-header--opened');
+            }
+        });
+    }
+}());

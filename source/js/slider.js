280b764c9f942e889956ec21e1adbdf7505765ad
Anastasya <aamokeeva@yandex.ru>
2021-05-07 20:44:15 +0300

    Делает слайдер для Галереи на планшете и мобильном

---
+++ source/js/slider.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// слайдер изображений для секции gallery
+
+(function () {
+
+  var slides = document.querySelectorAll('.slider__item');
+  var buttonLeft = document.querySelector('.slider__button--left');
+  var buttonRight = document.querySelector('.slider__button--right');
+
+  var slideIndex = 1; // начальный индекс слайда по умолчанию
+
+  if (slides) {
+    var slidesArray = Array.from(slides);
+
+    var showSlides = function (n) {
+      if (n > slidesArray.length) {
+        slideIndex = 1;
+      }
+
+      if (n < 1) {
+        slideIndex = slidesArray.length;
+      }
+
+      slidesArray.forEach(function (item) {
+        item.classList.remove('slider__item--active');
+      });
+
+      slidesArray[slideIndex - 1].classList.add('slider__item--active');
+    };
+  }
+
+  if (buttonLeft) {
+    buttonLeft.addEventListener('click', function () {
+      showSlides(slideIndex -= 1);
+    });
+  }
+
+  if (buttonRight) {
+    buttonRight.addEventListener('click', function () {
+      showSlides(slideIndex += 1);
+    });
+  }
+
+})();
